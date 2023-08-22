de2af3e1afb152a3ec73062d1140f85c68ef56aa
Oninsssss <134608756+Oninsssss@users.noreply.github.com>
2023-08-22 18:14:49 +0800

    Update reports.php

--- reports.php
+++ reports.php
@@ -1,5 +1,3 @@
-/*reports.php*/
-
 <?php 
 
 /*require_once('config/db.php');
@@ -13,6 +11,7 @@ require_once 'config/functions.php';
 $result=display_data();
 $result2=display_data2();
 $result3=display_data3();
+$result4=display_data4();
 ?>
 
 
@@ -24,156 +23,197 @@ $result3=display_data3();
     <link rel="stylesheet" href="./css/reports.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+    
     <title>Reports</title>
 </head>
 <body style="background-color: #087b81;">
-    <div class="container " style="margin-top:80px;">
+<div class="container" style="margin-top:100px; ">
         <button class="tablink" onclick="openPage('Home', this, 'white')"id="defaultOpen">Delivery Receipt</button>
         <button class="tablink" onclick="openPage('News', this, 'white')" >Stock Receipt</button>
         <button class="tablink" onclick="openPage('Contact', this, 'white')">Customers</button>
         <button class="tablink" onclick="openPage('About', this, 'white')">Orders</button>
-        
+
         <div id="Home" class="tabcontent">
-        <div class="container">
-        <div class="row">
-            <div class="col">
-                <div class="card">
-                    <div class="card-header">
-                        <h2>Delivery Reports</h2>
-                    </div>
-                    <div class="card-body">
-                        <table class="table table-bordered text-center">
-                            <tr>
-                                <td>DR No.</td>
-                                <td>Product Code</td>
-                                <td>Description</td>
-                                <td>Price</td>
-                                <td>Quantity</td> 
-                                <td>Total</td>
-                                <td>Delete</td>
-                            </tr>
-                                <?php 
-
-
-                                    while($row = mysqli_fetch_assoc($result)){
-                                ?>
-                                    <td><?php echo $row['DRNo.']; ?></td>
-                                    <td><?php echo $row['productCode']; ?></td>
-                                    <td><?php echo $row['Description']; ?></td>
-                                    <td><?php echo $row['Price']; ?></td>
-                                    <td><?php echo $row['Quantity']; ?></td>
-                                    <td><?php echo $row['Total']; ?></td>
-                                    <td><a href="#" class="btn btn-danger">Delete</a></td>
-                                </tr>        
-                                <?php
-                                    }
-
-
-                                ?>
-                            
-                        </table>
+          <div class="container" >
+            <div class="row" >
+                <div class="col-lg-12 col-md-12 col-sm-12" style="background-color:white;">
+                    <div class="card" >
+                        <div class="card-header" style="background-color:white;">
+                            <h2 style="background-color:white;">Delivery Reports</h2>
+                        </div>
+                        <div class="card-body" style="background-color:white;">
+                            <table class="table table-bordered text-center">
+                                <tr>
+                                    <td>DR No.</td>
+                                    <td>Product Code</td>
+                                    <td>Description</td>
+                                    <td>Price</td>
+                                    <td>Quantity</td> 
+                                    <td>Total</td>
+                                </tr>
+                                    <?php 
+                                
+    
+                                        while($row = mysqli_fetch_assoc($result)){
+                                    ?>
+                                        <td><?php echo $row['DRNo.']; ?></td>
+                                        <td><?php echo $row['productCode']; ?></td>
+                                        <td><?php echo $row['Description']; ?></td>
+                                        <td><?php echo $row['Price']; ?></td>
+                                        <td><?php echo $row['Quantity']; ?></td>
+                                        <td><?php echo $row['Total']; ?></td>
+                                    </tr>        
+                                    <?php
+                                        }
+    
+    
+                                    ?>
+                                
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         </div>
         </div>
         
         <div id="News" class="tabcontent">
-        <div class="container">
-        <div class="row">
-            <div class="col">
-                <div class="card">
-                    <div class="card-header">
-                        <h2>Stock Receipt</h2>
-                    </div>
-                    <div class="card-body">
-                        <table class="table table-bordered text-center">
-                            <tr>
-                                <td>Product Code</td>
-                                <td>Description</td>
-                                <td>Price</td>
-                                <td>Quantity</td> 
-                                <td>Total</td>
-                                <td>Delete</td>
-                            </tr>
-                                <?php
-
-
-                                    while($row = mysqli_fetch_assoc($result2)){
-                                ?>
-                                    <td><?php echo $row['productCode']; ?></td>
-                                    <td><?php echo $row['Description']; ?></td>
-                                    <td><?php echo $row['Price']; ?></td>
-                                    <td><?php echo $row['Quantity']; ?></td>
-                                    <td><?php echo $row['Total']; ?></td>
-                                    <td><a href="#" class="btn btn-danger">Delete</a></td>
-                                </tr>        
-                                <?php
-                                    }
-
-
-                                ?>
-                            
-                        </table>
+            <div class="container">
+                <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12" style="background-color:white;">
+                    <div class="card" >
+                        <div class="card-header" style="background-color:white;">
+                            <h2 style="background-color:white;">Stock Receipt</h2>
+                            </div>
+                            <div class="card-body" style="background-color:white;">
+                                <table class="table table-bordered text-center">
+                                    <tr>
+                                        <td>Product Code</td>
+                                        <td>Description</td>
+                                        <td>Price</td>
+                                        <td>Quantity</td> 
+                                        <td>Total</td>
+                                    </tr>
+                                        <?php 
+        
+        
+                                            while($row = mysqli_fetch_assoc($result2)){
+                                        ?> 
+                                        <tr>
+                                            <td><?php echo $row['productCode']; ?></td>
+                                            <td><?php echo $row['Description']; ?></td>
+                                            <td><?php echo $row['Price']; ?></td>
+                                            <td><?php echo $row['Quantity']; ?></td>
+                                            <td><?php echo $row['Total']; ?></td>
+                                        </tr>        
+                                        <?php
+                                            }
+        
+        
+                                        ?>
+                                    
+                                </table>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        </div>
         </div>
         
         <div id="Contact" class="tabcontent">
-        <div class="container">
-        <div class="row">
-            <div class="col">
-                <div class="card">
-                    <div class="card-header">
-                        <h2>Customer List</h2>
-                    </div>
-                    <div class="card-body">
-                        <table class="table table-bordered text-center">
-                            <tr>
-                                <td>Store Code</td>
-                                <td>Store Name</td>
-                                <td>Customer Name</td>
-                                <td>Address</td>
-                                <td>Contact No.</td> 
-                                <td>Delete</td>
-                            </tr>
-                                <?php
-
-                                    while($row = mysqli_fetch_assoc($result3)){
-                                ?>
-                                    <td><?php echo $row['storeCode']; ?></td>
-                                    <td><?php echo $row['storeName']; ?></td>
-                                    <td><?php echo $row['customerName']; ?></td>
-                                    <td><?php echo $row['Address']; ?></td>
-                                    <td><?php echo $row['contactNumber']; ?></td>
-                                    <td><a href="#" class="btn btn-danger">Delete</a></td>
-                                </tr>        
-                                <?php
-                                    }
-
-
-                                ?>
-                            
-                        </table>
+            <div class="container">
+                <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12" style="background-color:white;">
+                    <div class="card" >
+                        <div class="card-header" style="background-color:white;">
+                            <h2 style="background-color:white;">Customer List</h2>
+                            </div>
+                            <div class="card-body" style="background-color:white;">
+                                <table class="table table-bordered text-center">
+                                    <tr>
+                                        <td>Customer Name</td>
+                                        <td>In Charge</td>
+                                        <td>Customer Name</td>
+                                        <td>Address</td> 
+                                        <td>Contact Number</td>
+                                    </tr>
+                                        <?php 
+        
+        
+                                            while($row = mysqli_fetch_assoc($result3)){
+                                        ?> 
+                                            <td><?php echo $row['storeName']; ?></td>
+                                            <td><?php echo $row['inCharge']; ?></td>
+                                            <td><?php echo $row['customerName']; ?></td>
+                                            <td><?php echo $row['Address']; ?></td>
+                                            <td><?php echo $row['contactNo']; ?></td>
+                                        </tr>        
+                                        <?php
+                                            }
+        
+        
+                                        ?>
+                                    
+                                </table>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        </div>
-        </div>
         
         <div id="About" class="tabcontent">
-          <h3 class="text-center">Orders</h3>
+            <div class="container">
+                <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12" style="background-color:white;">
+                    <div class="card" >
+                        <div class="card-header" style="background-color:white;">
+                            <h2 style="background-color:white;">Orders</h2>
+                            </div>
+                            <div class="card-body" style="background-color:white;">
+                                <table class="table table-bordered text-center">
+                                    <tr>
+                                        <td>Store Name</td>
+                                        <td>DR Number</td>
+                                        <td>Status</td>
+                                        <td>Quantity</td> 
+                                    </tr>
+                                        <?php 
+        
+        
+                                            while($row = mysqli_fetch_assoc($result4)){
+                                        ?> 
+                                            <td><?php echo $row['storeName']; ?></td>
+                                            <td><?php echo $row['DRNo']; ?></td>
+                                            <td><?php echo $row['Status']; ?></td>
+                                            <td><?php echo $row['Quantity']; ?></td>
+                                        </tr>        
+                                        <?php
+                                            }
+        
+        
+                                        ?>
+                                    
+                                </table>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
         </div>
 
+</div>
+
+    <input value='Print' type='button' class='btn btn-light' style='float:right; margin: 70px; margin-top: 20px;' onclick='handlePrint()' />
+      <script type="text/javascript">
+         const handlePrint = () => {
+            var actContents = document.body.innerHTML;
+            document.body.innerHTML = actContents;
+            window.print();
+         }
+      </script>
+
         <script src="javascript/reports.js"></script>    
-
-
-    
-    <script src="javascript/reports.js"></script>    
 </body>
 </html>
