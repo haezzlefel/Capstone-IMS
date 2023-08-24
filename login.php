16645e81e16463caa31613e9e442013d7e5798b6
Charlie M. Abuloc <charlie.abuloc@myyahoo.com>
2023-08-24 16:03:53 +0800

    init

---
+++ login.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>IMS login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css"> 
+  </head>
+  <body>
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-6">
+          <h1>Inventory Management System</h1>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-6">
+
+          <form>
+
+          <div class="form-floating mb-3">
+  <input type="username" class="form-control" id="username" placeholder="">
+  <label for="username">Enter Username</label>
+</div>
+<div class="form-floating">
+  <input type="password" class="form-control" id="password" placeholder="">
+  <label for="password">Enter Password</label>
+</div>
+
+<div class="d-grid gap-2 d-flex justify-content-between">
+<a href="#" class="btn btn-primary" >Register</a>
+  <button class="btn btn-primary" type="submit">Button</button>
+</div>
+</form>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
